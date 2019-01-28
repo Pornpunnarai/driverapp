ca5e1f78444faea8e682b689afcc743c054a1b64
Pornpunnarai <kudokung_w@hotmail.com>
2019-01-28 14:21:35 +0700

    fix login page

--- lib/app_screens/loginpage.dart
+++ lib/app_screens/loginpage.dart
@@ -1,120 +1,123 @@
-import 'package:flutter/material.dart';
-
-class LoginPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.only(left: 15.0, top: 40.0, right: 15.0),
-        alignment: Alignment.center,
-        color: Colors.blueGrey,
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                    child: Text(
-                  'MacdoNal',
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                      fontFamily: 'Sarabun',
-                      fontWeight: FontWeight.w300,
-                      fontSize: 30.0,
-                      fontStyle: FontStyle.italic),
-                )),
-                Expanded(
-                  child: Text(
-                    'Bakery Nana Gunger',
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                        fontFamily: 'Sarabun',
-                        fontWeight: FontWeight.w300,
-                        fontSize: 25.0,
-                        fontStyle: FontStyle.italic),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                    child: Text(
-                  'หกดาสหกวาหวสห่ืสา',
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                      fontFamily: 'Sarabun',
-                      fontWeight: FontWeight.w300,
-                      fontSize: 30.0,
-                      fontStyle: FontStyle.italic),
-                )),
-                Expanded(
-                  child: Text(
-                    'ห่ืสห่ืไดวหาวส่',
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                        fontFamily: 'Sarabun',
-                        fontWeight: FontWeight.w300,
-                        fontSize: 25.0,
-                        fontStyle: FontStyle.italic),
-                  ),
-                ),
-              ],
-            ),
-            ImageAsset(),
-            Button(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ImageAsset extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    AssetImage assetImage = AssetImage('images/car-icon-red.png');
-    Image image = Image(
-      image: assetImage,
-      width: 100.0,
-    );
-    return Container(
-      child: image,
-    );
-  }
-}
-
-class Button extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 30.0),
-      width: 200.0,
-      height: 40.0,
-      child: RaisedButton(
-          color: Colors.green,
-          child: Text(
-            'Accept',
-            style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white,
-                fontFamily: 'Sarabun',
-                fontWeight: FontWeight.w700),
-          ),
-          elevation: 6.0,
-          onPressed: () => statusChange(context)),
-    );
-  }
-
-  void statusChange(BuildContext context){
-    var alearDialog = AlertDialog(
-      title: Text('Avaliable'),
-      content: Text('still working'),
-    );
-
-    showDialog(
-        context:context,
-        builder: (BuildContext context) => alearDialog
-
-    );
-  }
-}
+//import 'package:flutter/material.dart';
+//
+//class LoginPage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Container(
+//        padding: EdgeInsets.only(left: 15.0, top: 40.0, right: 15.0),
+//        alignment: Alignment.center,
+//        color: Colors.blueGrey,
+//        child: Column(
+//          children: <Widget>[
+//            Row(
+//              children: <Widget>[
+//                Expanded(
+//                    child: Text(
+//                      'MacdoNal',
+//                      textDirection: TextDirection.ltr,
+//                      style: TextStyle(
+//                          fontFamily: 'Sarabun',
+//                          fontWeight: FontWeight.w300,
+//                          fontSize: 30.0,
+//                          fontStyle: FontStyle.italic),
+//                    )),
+//                Expanded(
+//                  child: Text(
+//                    'Bakery Nana Gunger',
+//                    textDirection: TextDirection.ltr,
+//                    style: TextStyle(
+//                        fontFamily: 'Sarabun',
+//                        fontWeight: FontWeight.w300,
+//                        fontSize: 25.0,
+//                        fontStyle: FontStyle.italic),
+//                  ),
+//                ),
+//              ],
+//            ),
+//            Row(
+//              children: <Widget>[
+//                Expanded(
+//                    child: Text(
+//                      'หกดาสหกวาหวสห่ืสา',
+//                      textDirection: TextDirection.ltr,
+//                      style: TextStyle(
+//                          fontFamily: 'Sarabun',
+//                          fontWeight: FontWeight.w300,
+//                          fontSize: 30.0,
+//                          fontStyle: FontStyle.italic),
+//                    )),
+//                Expanded(
+//                  child: Text(
+//                    'ห่ืสห่ืไดวหาวส่',
+//                    textDirection: TextDirection.ltr,
+//                    style: TextStyle(
+//                        fontFamily: 'Sarabun',
+//                        fontWeight: FontWeight.w300,
+//                        fontSize: 25.0,
+//                        fontStyle: FontStyle.italic),
+//                  ),
+//                ),
+//              ],
+//            ),
+//            ImageAsset(),
+//            Button(),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
+//
+//class ImageAsset extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    AssetImage assetImage = AssetImage('images/car-icon-red.png');
+//    Image image = Image(
+//      image: assetImage,
+//      width: 100.0,
+//    );
+//    return Container(
+//      child: image,
+//    );
+//  }
+//}
+//
+//class Button extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext contex t) {
+//    return Container(
+//      margin: EdgeInsets.only(top: 30.0),
+//      width: 200.0,
+//      height: 40.0,
+//      child: RaisedButton(
+//          color: Colors.green,
+//          child: Text(
+//            'Accept',
+//            style: TextStyle(
+//                fontSize: 20.0,
+//                color: Colors.white,
+//                fontFamily: 'Sarabun',
+//                fontWeight: FontWeight.w700),
+//          ),
+//          elevation: 6.0,
+//          onPressed: () {
+//            //action
+////            statusChange();
+//          }),
+//    );
+//  }
+//
+//  void statusChange(BuildContext context){
+//    var alearDialog = AlertDialog(
+//      title: Text('Avaliable'),
+//      content: Text('still working'),
+//    );
+//
+//    showDialog(
+//        context:context,
+//        builder: (BuildContext context) => alearDialog
+//
+//    );
+//  }
+//}
--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,112 @@
-import 'package:driverapp/app_screens/loginpage.dart';
 import 'package:flutter/material.dart';
+import 'package:driverapp/app_screens/homepage.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: "Driver",
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(title:Text('Driver'),),
-      body: LoginPage(),
-    ),
-  ));
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Driver Login App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginForm(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
-//Widget getListView() {
-//  var listView = ListView(
-//    children: <Widget>[
-//      ListTile(
-//        leading: Icon(Icons.landscape),
-//        title: Text('Landscape'),
-//        subtitle: Text('Beautiful View!'),
-//        trailing: Icon(Icons.wb_sunny),
-//        onTap: (){
-//          debugPrint('Tapped');
-//        },
-//      ),
-//      ListTile(
-//        leading: Icon(Icons.label_important),
-//        title: Text('Window'),
-//        subtitle: Text('Beautiful View!'),
-//        trailing: Icon(Icons.wb_sunny),
-//      ),
-//      ListTile(
-//        leading: Icon(Icons.phone),
-//        title: Text('Phone'),
-//        subtitle: Text('Beautiful View!'),
-//        trailing: Icon(Icons.wb_sunny),
-//      ),
-//      ListTile(
-//        leading: Icon(Icons.phone),
-//        title: Text('Phone'),
-//        subtitle: Text('Beautiful View!'),
-//        trailing: Icon(Icons.wb_sunny),
-//      ),
-//      ListTile(
-//        leading: Icon(Icons.phone),
-//        title: Text('Phone'),
-//        subtitle: Text('Beautiful View!'),
-//        trailing: Icon(Icons.wb_sunny),
-//      ),
-//      ListTile(
-//        leading: Icon(Icons.phone),
-//        title: Text('Phone'),
-//        subtitle: Text('Beautiful View!'),
-//        trailing: Icon(Icons.wb_sunny),
-//      ),
-//      ListTile(
-//        leading: Icon(Icons.phone),
-//        title: Text('Phone'),
-//        subtitle: Text('Beautiful View!'),
-//        trailing: Icon(Icons.wb_sunny),
-//      ),
-//
-//      Text('another element in list'),
-//      Container(color: Colors.blueGrey, height: 30.0,)
-//    ],
-//  );
-//
-//  return listView;
-//}
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+
+
+
+class _LoginFormState extends State<LoginForm> {
+  final _minimumPadding = 5.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+//      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text('Driver Login'),
+//        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: ListView(
+          children: <Widget>[
+            Logo(),
+            Padding(
+              padding: EdgeInsets.all(_minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Enter your username',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0)
+                  )
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(_minimumPadding),
+              child: TextField(
+                  keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('Login',
+                      style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => HomeScreen(),
+                    ));
+                  },
+
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+
+  }
+  Widget Logo(){
+     return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        new Container(
+          margin: EdgeInsets.all(_minimumPadding * 10),
+          height: 100.0,
+          width: 100.0,
+          decoration: new BoxDecoration(
+              borderRadius: new BorderRadius.circular(50.0),
+              color: Colors.red),
+          child: new Icon(
+            Icons.directions_bus,
+            color: Colors.white,
+            size: 80.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
