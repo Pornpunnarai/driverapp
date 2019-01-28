ba77a5bb02897fd11dc4bab0007ef5e32a4990a7
paruj <paruj_lertjiranuwat@cmu.ac.th>
2019-01-28 15:40:24 +0700

    add authethication and fix main

--- lib/app_screens/loginpage.dart
+++ lib/app_screens/loginpage.dart
@@ -1,123 +1,91 @@
-//import 'package:flutter/material.dart';
-//
-//class LoginPage extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Center(
-//      child: Container(
-//        padding: EdgeInsets.only(left: 15.0, top: 40.0, right: 15.0),
-//        alignment: Alignment.center,
-//        color: Colors.blueGrey,
-//        child: Column(
-//          children: <Widget>[
-//            Row(
-//              children: <Widget>[
-//                Expanded(
-//                    child: Text(
-//                      'MacdoNal',
-//                      textDirection: TextDirection.ltr,
-//                      style: TextStyle(
-//                          fontFamily: 'Sarabun',
-//                          fontWeight: FontWeight.w300,
-//                          fontSize: 30.0,
-//                          fontStyle: FontStyle.italic),
-//                    )),
-//                Expanded(
-//                  child: Text(
-//                    'Bakery Nana Gunger',
-//                    textDirection: TextDirection.ltr,
-//                    style: TextStyle(
-//                        fontFamily: 'Sarabun',
-//                        fontWeight: FontWeight.w300,
-//                        fontSize: 25.0,
-//                        fontStyle: FontStyle.italic),
-//                  ),
-//                ),
-//              ],
-//            ),
-//            Row(
-//              children: <Widget>[
-//                Expanded(
-//                    child: Text(
-//                      'หกดาสหกวาหวสห่ืสา',
-//                      textDirection: TextDirection.ltr,
-//                      style: TextStyle(
-//                          fontFamily: 'Sarabun',
-//                          fontWeight: FontWeight.w300,
-//                          fontSize: 30.0,
-//                          fontStyle: FontStyle.italic),
-//                    )),
-//                Expanded(
-//                  child: Text(
-//                    'ห่ืสห่ืไดวหาวส่',
-//                    textDirection: TextDirection.ltr,
-//                    style: TextStyle(
-//                        fontFamily: 'Sarabun',
-//                        fontWeight: FontWeight.w300,
-//                        fontSize: 25.0,
-//                        fontStyle: FontStyle.italic),
-//                  ),
-//                ),
-//              ],
-//            ),
-//            ImageAsset(),
-//            Button(),
-//          ],
-//        ),
-//      ),
-//    );
-//  }
-//}
-//
-//class ImageAsset extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    AssetImage assetImage = AssetImage('images/car-icon-red.png');
-//    Image image = Image(
-//      image: assetImage,
-//      width: 100.0,
-//    );
-//    return Container(
-//      child: image,
-//    );
-//  }
-//}
-//
-//class Button extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext contex t) {
-//    return Container(
-//      margin: EdgeInsets.only(top: 30.0),
-//      width: 200.0,
-//      height: 40.0,
-//      child: RaisedButton(
-//          color: Colors.green,
-//          child: Text(
-//            'Accept',
-//            style: TextStyle(
-//                fontSize: 20.0,
-//                color: Colors.white,
-//                fontFamily: 'Sarabun',
-//                fontWeight: FontWeight.w700),
-//          ),
-//          elevation: 6.0,
-//          onPressed: () {
-//            //action
-////            statusChange();
-//          }),
-//    );
-//  }
-//
-//  void statusChange(BuildContext context){
-//    var alearDialog = AlertDialog(
-//      title: Text('Avaliable'),
-//      content: Text('still working'),
-//    );
-//
-//    showDialog(
-//        context:context,
-//        builder: (BuildContext context) => alearDialog
-//
-//    );
-//  }
-//}
+import 'package:flutter/material.dart';
+
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _minimumPadding = 5.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+//      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text('Driver Login'),
+//        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: ListView(
+          children: <Widget>[
+            Logo(),
+            Padding(
+              padding: EdgeInsets.all(_minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    labelText: 'Username',
+                    hintText: 'Enter your username',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(_minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('Login',
+                      style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+                  onPressed: (){
+//                    Navigator.push(context, MaterialPageRoute(
+//                      builder: (context) => HomeScreen(),
+//                    ));
+                  },
+
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+
+  }
+  Widget Logo(){
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        new Container(
+          margin: EdgeInsets.all(_minimumPadding * 10),
+          height: 100.0,
+          width: 100.0,
+          decoration: new BoxDecoration(
+              borderRadius: new BorderRadius.circular(50.0),
+              color: Colors.red),
+          child: new Icon(
+            Icons.directions_bus,
+            color: Colors.white,
+            size: 80.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:driverapp/app_screens/homepage.dart';
+import 'package:driverapp/app_screens/loginpage.dart';
 
 void main() => runApp(new MyApp());
 
@@ -18,95 +18,4 @@ class MyApp extends StatelessWidget {
 }
 
 
-class LoginForm extends StatefulWidget {
-  @override
-  _LoginFormState createState() => _LoginFormState();
-}
-
-
-
-
-class _LoginFormState extends State<LoginForm> {
-  final _minimumPadding = 5.0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-//      resizeToAvoidBottomPadding: false,
-      appBar: AppBar(
-        title: Text('Driver Login'),
-//        backgroundColor: Colors.transparent,
-      ),
-      body: Container(
-        margin: EdgeInsets.all(_minimumPadding * 2),
-        child: ListView(
-          children: <Widget>[
-            Logo(),
-            Padding(
-              padding: EdgeInsets.all(_minimumPadding),
-              child: TextField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  hintText: 'Enter your username',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0)
-                  )
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(_minimumPadding),
-              child: TextField(
-                  keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0)
-                    )
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  child: Text('Login',
-                      style: new TextStyle(fontSize: 20.0, color: Colors.white)),
-                  onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => HomeScreen(),
-                    ));
-                  },
-
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-
-  }
-  Widget Logo(){
-     return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        new Container(
-          margin: EdgeInsets.all(_minimumPadding * 10),
-          height: 100.0,
-          width: 100.0,
-          decoration: new BoxDecoration(
-              borderRadius: new BorderRadius.circular(50.0),
-              color: Colors.red),
-          child: new Icon(
-            Icons.directions_bus,
-            color: Colors.white,
-            size: 80.0,
-          ),
-        ),
-      ],
-    );
-  }
-}
 
