67a3c5a8b2c22d932c1c21a168b2275018bb393f
Pornpunnarai <kudokung_w@hotmail.com>
2019-02-04 16:02:45 +0700

    fix profile page

--- lib/app_screens/third_screen.dart
+++ lib/app_screens/third_screen.dart
@@ -6,7 +6,6 @@ class Third extends StatelessWidget {
 
   }
 
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -22,55 +21,79 @@ class Third extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(15.0),
-              padding: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10.0),border: Border.all(color: Colors.red)),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex:1,
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.account_circle, size: 50.0,),
-                      ],
-                    ),
+    body: new ListView(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: new Container(
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(color: Colors.red)
                   ),
-                  Expanded(
-                    flex: 3,
-                    child: Row(
-                      children: <Widget>[
-                       Text('Pawnpunnarai Saimoonkham', style: TextStyle(fontSize: 16.0),),
-//                 Text('+66 824960172')
-                      ],
-                    ),
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new Container(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                padding: const EdgeInsets.all(15.0),   
+                                child: Icon(Icons.account_circle, size: 150.0,),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text('Pawnpunnarai Saimoonkham', style: TextStyle(fontSize: 22.0),),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(5.0),  
+                                child: Text('+66 824960172', style: TextStyle(fontSize: 18.0),),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(5.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  border: Border.all(color: Colors.white),
+                                ),
+                                child:
+                                Text('งน7821 ชม', style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.w700),),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
 
-              ),
+                    children: <Widget>[
+                        new RaisedButton(
+                          padding: const EdgeInsets.all(8.0),
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          onPressed: logout,
+                          child: new Text("Logout", style: TextStyle(fontSize: 18.0, color: Colors.white, fontWeight: FontWeight.w700),),
+                        ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              child: Column(
-                  children: <Widget>[
-                    new Align(alignment: FractionalOffset.bottomCenter),
-                    new RaisedButton(
-                      padding: const EdgeInsets.all(8.0),
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      onPressed: logout,
-                      child: new Text("Logout", style: TextStyle(fontSize: 18.0),),
-                    ),
-                  ],
-              ),
-            ),
-     
-          ],
-        ),
-
-        ),
+          ),
+        )
+      ],
+    ),
     );
   }
 }
